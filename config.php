47170db07c13dd3cb40d4ce0e26ed2ba50b32710
popatjayesh <popat.jayesh1988@gmail.com>
2020-07-04 10:54:33 +0000

    strangerhub commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.quickdate.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | Quickdate - The Ultimate PHP Dating Platform
+// | Copyright (c) 2018 Quickdate. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$servername = "localhost";
+// MySQL Database User
+$dbusername = "root";
+// MySQL Database Password
+$dbpassword = "@Strangerhub2020#";
+// MySQL Database Name
+$dbname = "strangerhub";
+
+// Site URL
+$site_url = "http://strangerhub.com"; // e.g (http://example.com)
+
+$app = "QuickDating";
+$endpoint_v = "v1";
+$serverkey = "0c5febfd1e1af5b9263f9f49752a0192";
+
+// Purchase code
+$purchase_code = "03b5c7f9-9403-4a26-a055-63d297033d15"; // Your purchase code, don't give it to anyone.
+?>
